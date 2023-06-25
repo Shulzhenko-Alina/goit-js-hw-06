40defedeb117a15bc0ddcd2b326681cec5175259
Shulzhenko-Alina <122262739+Shulzhenko-Alina@users.noreply.github.com>
2023-06-25 18:30:54 +0200

    commit 2

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,11 @@
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+// Оновлюй інтерфейс новим значенням змінної counterValue.
+
+
+
+
+
 const counterValue = document.querySelector("#value");
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const decrementBtn = document.querySelector('[data-action="decrement"]');
--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,12 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), 
+// підставляє його поточне значення в span#name-output. Якщо інпут порожній, 
+// у спані повинен відображатися рядок "Anonymous".
+
+
+
+
+
+
 const refs = {
     input: document.querySelector('#name-input'),
     nameEl: document.querySelector('#name-output'),
